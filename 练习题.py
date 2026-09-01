3fb6d61f1a7bc0370845abc19d9b8d54abeaedf7
aduan699 <2845556280@qq.com>
2026-09-01 15:47:35 +0800

    初次提交

---
+++ 练习题.py
@@ -0,0 +1,20 @@
+# 第一题
+# li = []
+# [li.append(i) for i in range(2000,3201) if i%7==0 and i%5!=0]
+# print(li,sep = ',',end = '')
+# 第二题
+# li =list(map(int,input().split()))
+# res = []
+# for n in li:
+#     result =1
+#     for i in range(1,n+1):
+#         result *= i
+#     res.append(result)
+# for i in res:
+#     print(i,sep = ',',end = '')
+# # 第三题
+# dic = {}
+# n = int(input())
+# for i in range(1,n+1):
+#     dic[i] = i*i
+# print(dic)
