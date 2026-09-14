645b79fc434dc3e20c044a3e6e673a6a2e5dc24d
aduan699 <2845556280@qq.com>
2026-09-14 09:11:59 +0800

    测试一下添加

--- 练习题.py
+++ 练习题.py
@@ -1,9 +1,12 @@
 import threading
 import time
+from threading import Lock
+from multiprocessing import Process,Queue 
 # 第一题
 # li = []
 # [li.append(i) for i in range(2000,3201) if i%7==0 and i%5!=0]
 # print(li,sep = ',',end = '')
+
 # 第二题
 # li =list(map(int,input().split()))
 # res = []
@@ -14,9 +17,31 @@ import time
 #     res.append(result)
 # for i in res:
 #     print(i,sep = ',',end = '')
+
 # # 第三题
 # dic = {}
 # n = int(input())
 # for i in range(1,n+1):
 #     dic[i] = i*i
 # print(dic)
+
+# #用q队列实现进程间的通信的案例
+# def  producer(q):
+#     for i in range(5):
+#         q.put(i)
+#         print(f'{i}已经放入队列')
+# def consumer(q):
+#     while True:
+#         if q.empty():
+#             break
+#         else:
+#             for i in range(5):
+#                 print(f'从队列里面取出了{q.get()}')
+# if __name__ == '__main__':
+#     q = Queue()
+#     p1 = Process(target = producer,args = (q,))
+#     p2 = Process(target = consumer,args = (q,))
+#     p1.start()
+#     p1.join()
+#     p2.start()
+#     p2.join()
